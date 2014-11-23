b854e418fced02cfb14010d7f98ed5215447bada
Ondrej Vana <ondrej.vana@keboola.com>
2014-11-23 00:43:36 -0800

    feat: Add getDataFromPath()

--- src/Keboola/Utils/Utils.php
+++ src/Keboola/Utils/Utils.php
@@ -219,4 +219,36 @@ class Utils {
 		$string = str_replace(" ", "", $string);
 		return $ucfirst ? $string : lcfirst($string);
 	}
+
+	/**
+	 * Returns associative array or string of data stored in $path in the $data array
+	 *
+	 * @param string $path path/to/interesting/data
+	 * @param array|object $data The object containing data
+	 * @return mixed
+	 */
+	public static function getDataFromPath($path, $data, $separator = "/", $ignoreEmpty = true)
+	{
+		if (!empty($path) && $path != $separator) {
+			// TODO possibly add functions in the $path? Such as path/to/join(data) would do a join on that data..likely belongs to EX/(sub)Parser/TR
+			$str = explode($separator, $path);
+			// Step down into the $data object, iterate until the desired path is reached (if not empty)
+			foreach($str as $key) {
+				if (is_object($data) && property_exists($data, $key)) {
+					$data = $data->{$key};
+				} elseif (is_array($data) && isset($data[$key])) {
+					$data = $data[$key];
+				} else {
+					if ($ignoreEmpty == true) { // return if empty and ignore == true
+						$data = "";
+						break;
+					} else {
+						throw new UserException("Error parsing data. {$path} not found.");
+					}
+				}
+			}
+		}
+
+		return $data;
+	}
 }
