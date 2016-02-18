7796ee5a9aac3d7ee21c078c68669cf753aa2387
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2016-02-18 09:35:35 +0100

    add bootstrap with timezone

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+// Define path to application directory
+define('ROOT_PATH', __DIR__);
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+	realpath(ROOT_PATH . '/library'),
+	get_include_path(),
+)));
+ini_set('display_errors', true);
+
+require_once ROOT_PATH . '/vendor/autoload.php';
+date_default_timezone_set('UTC');
