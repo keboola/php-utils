f11c873d4c462916e0c0b560d3300389cc7de889
Ondrej Vana <ondrej.vana@keboola.com>
2016-02-17 16:16:36 -0800

    style: Replaced tabs with spaces

--- src/Keboola/Utils/Exception/Exception.php
+++ src/Keboola/Utils/Exception/Exception.php
@@ -3,24 +3,24 @@ namespace Keboola\Utils\Exception;
 
 class Exception extends \Exception {
 
-	protected $data = array();
+    protected $data = array();
 
-	/**
-	 * @return array
-	 */
-	public function getData()
-	{
-		return $this->data;
-	}
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
 
-	/**
-	 * @param array $data
-	 * @return $this
-	 */
-	public function setData(array $data)
-	{
-		$this->data = $data;
+    /**
+     * @param array $data
+     * @return $this
+     */
+    public function setData(array $data)
+    {
+        $this->data = $data;
 
-		return $this;
-	}
+        return $this;
+    }
 }
