df25774d5b8e2b5fa59bbae0cd81121c760fa0a8
Odin <odinuv@gmail.com>
2019-01-09 23:47:37 +0100

    refactor: move implementation to shared library

--- src/Keboola/Utils/sanitizeColumnName.php
+++ src/Keboola/Utils/sanitizeColumnName.php
@@ -2,8 +2,10 @@
 
 namespace Keboola\Utils;
 
+use Keboola\Utils\Sanitizer\ColumnNameSanitizer;
+
 /**
- * https://github.com/nette/nette/blob/master/Nette/Utils/Strings.php#L188
+ *https://github.com/nette/utils/blob/v2.5/src/Utils/Strings.php#L199
  * Converts to web safe characters [a-z0-9-] text.
  * @param  string  UTF-8 encoding
  * @param  string  allowed characters
@@ -12,21 +14,5 @@ namespace Keboola\Utils;
  */
 function sanitizeColumnName($s, $charlist = '_', $lower = false)
 {
-    $systemColumns = [
-        'oid',
-        'tableoid',
-        'xmin',
-        'cmin',
-        'xmax',
-        'cmax',
-        'ctid',
-    ];
-
-    $s = toAscii($s);
-    if ($lower) {
-        $s = strtolower($s);
-    }
-    $s = preg_replace('#[^a-z0-9' . preg_quote($charlist, '#') . ']+#i', '_', $s);
-    $s = trim($s, '_');
-    return in_array(strtolower($s), array_map('strtolower', $systemColumns)) ? "{$s}_" : $s;
+    return ColumnNameSanitizer::sanitize($s, $charlist, $lower);
 }
